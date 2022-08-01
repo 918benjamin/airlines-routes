32840dd9f2ac2120f658620af321c7f8918d5f60
Benjamin Perrault <benjamin.perrault@gmail.com>
2022-08-01 15:38:15 -0500

    Step 5: add pagination to table along with buttons for going up and down

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './App.css';
 import data from './data';
 import Table from './components/Table';
@@ -21,24 +21,55 @@ const columns = [
     {name: 'Destination Airport', property: 'dest'},
 ];
 
-const App = () => (
-  <div className="app container">
-  <header className="header">
-    <h1 className="title">Airline Routes</h1>
-  </header>
-  <section>
-    <p>Welcome to the App</p>
-  </section>
-    <Table
-      className="routes-table"
-      columns={columns}
-      rows={data.routes}
-      format={formatValue}
-    />
-  <section>
+const App = () =>  {
+  let [ firstRouteToShow, setFirstRouteToShow ] = useState(0)
+  const PER_PAGE = 73;
 
-  </section>
-</div>
-)
+  const incrementVisibleRoutes = (event) => {
+    setFirstRouteToShow(firstRouteToShow + PER_PAGE)
+  }
+
+  const decrementVisibleRoutes = (event) => {
+    setFirstRouteToShow(firstRouteToShow - PER_PAGE)
+  }
+
+  const topOfRange = function() {
+    return firstRouteToShow + PER_PAGE <= data.routes.length ?
+      firstRouteToShow + PER_PAGE :
+      data.routes.length
+  }
+
+  return (
+    <div className="app container">
+      <header className="header">
+        <h1 className="title">Airline Routes</h1>
+      </header>
+      <section>
+        <p>Welcome to the App</p>
+      </section>
+        <Table
+          className="routes-table"
+          columns={columns}
+          rows={data.routes}
+          format={formatValue}
+          rowsPerPage={PER_PAGE}
+          firstRow={firstRouteToShow}
+        />
+
+        <p>{`Showing ${firstRouteToShow+1}-${topOfRange()} of ${data.routes.length} routes`}</p>
+        <button
+          disabled={firstRouteToShow === 0 ? true : false}
+          onClick={decrementVisibleRoutes}
+        >
+        Previous Page
+        </button>
+        <button
+        disabled={firstRouteToShow + PER_PAGE >= data.routes.length ? true : false}
+        onClick={incrementVisibleRoutes}>Next Page</button>
+    </div>
+  )
+}
+
+
 
 export default App;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
-const Table = ({ columns, rows, format }) => {
+const Table = ({ columns, rows, format, rowsPerPage, firstRow }) => {
   return (
     <table>
       <thead>
         <tr>
           {columns.map(column => 
-            <th>{column.name}</th>
+            <th key={column.property}>{column.name}</th>
           )}
         </tr>
       </thead>
       <tbody>
-        {rows.map(row => 
-          <tr>
+        {rows.slice(firstRow, firstRow + rowsPerPage).map(row => 
+          <tr key={columns.map(column => row[column.property]).join('')}>
           {columns.map(column =>
-            <td>{format(column.property, row[column.property])}</td>
+            <td key={column.property}>{format(column.property, row[column.property])}</td>
           )}
           </tr>
         )}
