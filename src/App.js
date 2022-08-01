92b255f7468cac1d551f10bf5c10559ee41a1f10
Benjamin Perrault <benjamin.perrault@gmail.com>
2022-08-01 12:43:46 -0500

    Require data in our app file, added bootstrap, updated react and react-dom to latest

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Table } from 'react-bootstrap';
+import data from './data';
 
 const App = () => (
-  <div className="app">
+  <div className="app container">
   <header className="header">
     <h1 className="title">Airline Routes</h1>
   </header>
   <section>
-    <p>
-      Welcome to the app!
-    </p>
+    <p>Welcome to the App</p>
+  </section>
+  <section>
+    <Table>
+      <tbody>
+        {data.routes.map(route => 
+          <tr>
+            <td>Airline:{route.airline}</td>
+            <td>Source:{route.src}</td>
+            <td>Destination:{route.dest}</td>
+          </tr>
+        )}
+      </tbody>
+    </Table>
   </section>
 </div>
 )
